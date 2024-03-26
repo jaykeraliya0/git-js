43f2ebc5aaa4e46d20960931a36949a0cf06514d
Jay Keraliya <jaykeraliya0@gmail.com>
2024-03-26 12:32:46 +0530

    added cat-file

--- app/main.js
+++ app/main.js
@@ -1,11 +1,16 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require("zlib");
 
 const command = process.argv[2];
 
 switch (command) {
   case "init":
     createGitDirectory();
+    break;
+
+  case "cat-file":
+    catFile(process.argv[3]);
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -21,4 +26,17 @@ function createGitDirectory() {
     "ref: refs/heads/main\n"
   );
   console.log("Initialized git directory");
+}
+
+function catFile(blobSha) {
+  const dir = blobSha.slice(0, 2);
+  const file = blobSha.slice(2);
+
+  const objPath = path.join(__dirname, ".git", "objects", dir, file);
+  const data = fs.readFileSync(objPath);
+
+  const dataUnzipped = zlib.inflateSync(data);
+  const res = dataUnzipped.toString().split("\0");
+
+  process.stdout.write(res[1]);
 }
